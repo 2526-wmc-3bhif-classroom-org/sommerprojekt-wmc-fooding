c3bb6218d1ec5aff189843223f1f58358d42550b
markic <m.trkulja@students.htl-leonding.ac.at>
2026-03-29 13:15:41 +0200

    fixed bug and added a file because the router in the index.ts there was a InventoryView.vue that didnt existed and i added to fix it

---
+++ frontend/src/views/InventoryView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="inventory-view">
+    <h1>Bestand</h1>
+    <p>Willkommen zur Bestand-Seite!</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Import statements here if needed
+</script>
+
+<style scoped>
+.inventory-view {
+  padding: 2rem;
+}
+
+h1 {
+  color: #333;
+  margin-bottom: 1rem;
+}
+
+p {
+  color: #666;
+  font-size: 1rem;
+}
+</style>
