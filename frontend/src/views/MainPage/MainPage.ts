e0481c60cc34ad18c47f2d496dfad5fc7437cf56
l-marazovic <l.marazovic@students.htl-leonding.ac.at>
2026-03-30 00:13:02 +0200

    feat: implement theme toggle functionality for light and dark modes

--- frontend/src/views/MainPage/MainPage.ts
+++ frontend/src/views/MainPage/MainPage.ts
@@ -1,5 +1,30 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
+type ThemeMode = 'dark' | 'light'
 export default defineComponent({
-  name: 'MainPage'
-})
+  name: 'MainPage',
+  setup() {
+    const theme = ref('light'); // Standardwert 'light'
+    const toggleTheme = () => {
+      theme.value = theme.value === 'dark' ? 'light' : 'dark';
+    };
+
+    return {
+      theme,
+      toggleTheme,
+    };
+  },
+
+});
+
+
+const theme = ref<ThemeMode>('light')
+
+const toggleTheme = () => {
+  theme.value = theme.value === 'dark' ? 'light' : 'dark'
+  updateDocumentTheme()
+}
+
+const updateDocumentTheme = () => {
+  document.documentElement.setAttribute('data-theme', theme.value)
+}
