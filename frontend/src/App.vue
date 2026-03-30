61d9e0e9fd9b74183987729ca63a2b9a4b1efc50
lorenz-parzer <l.parzer@students.htl-leonding.ac.at>
2026-03-30 22:36:53 +0200

    edited paths

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import MainPage from '@/views/MainPage/MainPage.ts'
 </script>
 
 <template>
   <RouterView />
-  <MainPage />
 </template>
