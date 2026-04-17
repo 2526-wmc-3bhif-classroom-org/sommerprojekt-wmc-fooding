cc49c8ecf03da55f8741aa58de364531dd750116
lorenz-parzer <l.parzer@students.htl-leonding.ac.at>
2026-04-17 18:57:21 +0200

    now it runs again

--- backend/server/src/models/shopping_list_item/shopping-list-item-repository.ts
+++ backend/server/src/models/shopping_list_item/shopping-list-item-repository.ts
@@ -1,15 +1,11 @@
 import { DB } from "../../db/database";
-import type { Database as DatabaseType } from "better-sqlite3";
 
 export class ShoppingListItemRepository {
-    private db: DatabaseType;
-
-    constructor() {
-        this.db = DB.createDBConnection();
+    private static get db() {
+        return DB.getConnection();
     }
 
-
-    public addItem(userId: number, productId: number, recipeId: number, quantity: number): void {
+    public static addItem(userId: number, productId: number, recipeId: number, quantity: number): void {
         this.db.prepare(`
             INSERT INTO shopping_list_items (user_id, product_id, recipe_id, quantity, checked)
             VALUES (?, ?, ?, ?, 0)
